fe6eb138d0122d96879db27ed4ab50515d463b66
Evan Henley <evan.henley@gmail.com>
2014-10-12 01:32:31 -0500

    Update read-em-and-weep.js

--- read-em-and-weep.js
+++ read-em-and-weep.js
@@ -1,14 +1,16 @@
 function read(hand) {
-    var royal = 'A♠, K♠, Q♠, J♠, 10♠';
     return {
-        and: function(weep) {
-            return hand.join(', ') === royal ? weep : "I've seen better.";
+        and: function(fn) {
+            fn.call(hand);
         }
     };
 }
 
 var em = ['A♠', 'K♠', 'Q♠', 'J♠', '10♠'];
-var weep = 'Boo hoo.';
+var weep = function() {
+    var royal = 'A♠, K♠, Q♠, J♠, 10♠';
+    console.log(this.join(', ') === royal ? 'Boo hoo.' : "I've seen better.");
+};
 
 read(em).and(weep);
-// => 'Boo hoo.'
+// => Boo hoo.
